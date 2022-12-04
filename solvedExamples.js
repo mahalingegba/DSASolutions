b73fc16294a5dbf59db6e55250c46e2e407e28e7
mahalingegba <95224219+mahalingegba@users.noreply.github.com>
2022-12-04 23:18:10 +0530

    Add files via upload
    updated solution.

--- solvedExamples.js
+++ solvedExamples.js
@@ -168,3 +168,104 @@ var isPalindrome = function (x) {
   }
   return true;
 };
+
+//3
+var isPalindrome = function (s) {
+  s = s.replace(/[^a-zA-Z0-9]/g, "");
+  s = s.toLowerCase();
+
+  return s == s.split("").reverse().join("");
+};
+
+//7.Merging sorted Arrays
+var merge = function (nums1, m, nums2, n) {
+  for (let i = 0; i < n; i++) {
+    nums1[i + m] = nums2[i];
+  }
+  nums1.sort((a, b) => a - b);
+};
+
+//8.Anagram
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  let s1 = s.split("").sort().join("");
+  let t1 = t.split("").sort().join("");
+
+  if (s1 === t1) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//9.reverse string
+var reverseString = function (s) {
+  let left = 0,
+    right = s.length - 1;
+
+  while (left < right) {
+    temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+
+    left++;
+    right--;
+  }
+};
+
+//10.1 : Contains Duplicates
+var containsDuplicate = function (nums) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (Object.values(map).includes(nums[i])) {
+      return true;
+    } else {
+      map[i] = nums[i];
+    }
+  }
+  return false;
+};
+
+console.log(containsDuplicate([3, 5, 7]));
+
+//10.2 : Contains Duplicates
+
+var containsDuplicate = function (nums) {
+  return nums.length !== new Set(nums).size;
+};
+
+//11. longes substring
+var longestCommonPrefix = function (strs) {
+  if (strs.length == 0) return "";
+  let r = "";
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 0; j < strs.length; j++) {
+      if (strs[0][i] !== strs[j][i]) return r;
+    }
+    r += strs[0][i];
+  }
+  return r;
+};
+let strs = ["flower", "flow", "flight"];
+console.log(longestCommonPrefix(strs));
+//12.Valid paranthesis
+var isValid = function (s) {
+  if (s.length <= 1) return false;
+  let stack = [];
+  let hash = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    if (hash[s[i]]) stack.push(hash[s[i]]); //s[i] means { //hash[s[i]] means }
+    else if (s[i] !== stack.pop()) return false;
+  }
+  return stack.length === 0 ? true : false;
+};
+let s = "[({{{}}})]";
+console.log(isValid(s));
